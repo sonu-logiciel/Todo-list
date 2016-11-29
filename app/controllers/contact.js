5623556745fc9f501c89314c64f46bf90bbbc6f1
sonu-logiciel <sonu.mourya@logicielsolution.co.in>
2016-11-30 01:24:07 +0530

    added contact as compare to Home

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	email: '',
+	message:'',
+
+	isValidEmail: Ember.computed.match('email', /^.+@.+\..+$/),
+	isMessageEnoughLong: Ember.computed.gte('message.length', 5),
+
+	isValid: Ember.computed.and('isValidEmail', 'isMessageEnoughLong'),
+	isNotValid: Ember.computed.not('isValid'),
+	actions:{
+		sendResponse(){
+			alert(`we will contact you:${this.get('email')}`);
+			this.set('responseAlert',`your message are save we will contact you: ${this.get('email')} `);
+			this.set('message','');
+			this.set('email','');
+		}
+	}
+
+
 });

c08b389b5c294f0b5366f61319bb33ab205d5e16
umesh.sharma <umesh.sharma@logicielsolutions.co.in>
2016-11-29 16:24:34 +0530

    add function to enable button on etering text in text box

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,12 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isDisabled:true,
+  // isDisabled:true,
   emailAddress:'',
-  actualEmailAddress:Ember.computed('emailAddress',function(){
-    console.log('acutalemailAdree:',this.get('emailAddress'));
-  }),
-  emailAddressChange:Ember.observer('emailAddress',function(){
-    console.log('emailAddressChange:',this.get('emailAddress'));
+  // actualEmailAddress:Ember.computed('emailAddress',function(){
+  //   console.log('acutalemailAdree:',this.get('emailAddress'));
+  // }),
+  // emailAddressChange:Ember.observer('emailAddress',function(){
+  //   console.log('emailAddressChange:',this.get('emailAddress'));
+  // }),
+  isDisabled:Ember.computed('emailAddress',function(){
+    return this.get('emailAddress')==='';
   })
 });

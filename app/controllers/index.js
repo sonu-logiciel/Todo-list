53fc219f3a3850222794d4d7fe20493ec918febc
sonu-logiciel <sonu.mourya@logicielsolution.co.in>
2016-11-30 03:14:01 +0530

    added admin panel to show id and emails to admin

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -14,15 +14,20 @@ export default Ember.Controller.extend({
   isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
   isDisabled: Ember.computed.not('isValid'),
   actions:{
-    saveInitations(){
-      const email=this.get('emailAddress');
-  
-      const newInvitation=this.store.createRecord('invitation',{email:email});
-      newInvitation.save();
-      
-      this.set('responseMessage',`thank you we just save your EmailAdress:${this.get('emailAddress')}`);
-      this.set('emailAddress','');
+    saveInvitation() {
+      const email = this.get('emailAddress');
+
+      const newInvitation = this.store.createRecord('invitation', {
+        email: email
+      });
+
+      newInvitation.save().then((response) => {
+        this.set('responseMessage', `Thank you! We saved your email address with the following id: ${response.get('id')}`);
+        this.set('emailAddress', '');
+      });
+
     }
+
   }
 
 });

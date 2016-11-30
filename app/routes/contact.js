bdcfde1e372d534129b77f61476fd26a2e9079dc
umesh.sharma <umesh.sharma@logicielsolutions.co.in>
2016-11-30 17:47:40 +0530

    added crud functionality in library

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -8,14 +8,19 @@ export default Ember.Route.extend({
     sendResponse(newContact){
       newContact.save().then(()=>
         this.controller.set('responseAlert','Your message are save we will contact you shortly'));
+        setTimeout(function(){
+            window.location.reload(true);
+        }, 3000);
     },
     willTransition(){
+      console.log(this.set('newContact',''));
       let model = this.controller.get('model');
 
       if (model.get('isNew')) {
         model.destroyRecord();
       }
       this.controller.set('responseAlert', false);
+
     }
   }
 });

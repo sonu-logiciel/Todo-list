e363619f5cfdefb01c352c20a35258bd3488dc73
umesh.sharma <umesh.sharma@logicielsolutions.co.in>
2016-11-30 15:47:43 +0530

    move contact controller functioality to route and model

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -2,6 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    return  this.store.findAll('contact');
+    return this.store.createRecord('contact');
+  },
+  actions:{
+    sendResponse(newContact){
+      newContact.save().then(()=>
+        this.controller.set('responseAlert','Your message are save we will contact you shortly'));
+    },
+    willTransition(){
+      let model = this.controller.get('model');
+
+      if (model.get('isNew')) {
+        model.destroyRecord();
+      }
+      this.controller.set('responseAlert', false);
+    }
   }
 });
